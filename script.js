67256cabee6d94877b7d4329765b2399cf0fc154
Jerome Decinco <57111980+CodeKaito@users.noreply.github.com>
2023-11-20 12:15:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -287,33 +287,34 @@ tombolaMagica(computer, user);
 //* ARRAY:
 //? Ex.1 (Easy) - Realizzare un programma che costruisca un array di 20 elementi. Ogni elemento dev'essere pari al proprio indice nell'array.
 // Es. [0, 1, 2, 3, 4, 5 ... 19]
-
-// for (let i = 0; i < 20; i++) {
-//     console.log(i);
-// }
+let result = [];
+for (let i = 1; i <= 20; i++) {
+    result.push(i);
+}
+console.log(result);
 
 //? Ex.2 (Medium) - Realizzare un programma che, dato un array di numeri interi, stampi in console la media di tutti i valori.
 // N.B. Utilizza solo i concetti visti a lezione, niente scorciatoie!
 
-// let array = [1,2,3,4];
-// let result = 0;
-// for (let i = 0; i < array.length; i++) {
-//     result += array[i]/2;
-// }
-// console.log(result);
+let array = [1,2,3,4];
+let result = 0;
+for (let i = 0; i < array.length; i++) {
+    result += array[i]/2;
+}
+console.log(result);
 
 //? Ex.3 (Medium) - Realizzare un programma che rimuova i multipli dispari di 3 dall'array riportato sotto.
 // N.B. 18 è un multiplo pari di 3, 21 è un multiplo dispari di 3.
 // let array = [20, 31, 15, 78, 48, 27, 61, 51]; 
 
-// let array = [20, 31, 15, 78, 48, 27, 61, 51];
-// let result = [];
-// for (let i = 0; i < array.length; i++) {
-//     if (array[i] % 3 === 0) {
-//         result.push(array[i]);
-//     }
-// }
-// console.log(result);
+let array = [20, 31, 15, 78, 48, 27, 61, 51];
+let result = [];
+for (let i = 0; i < array.length; i++) {
+    if (array[i] % 3 === 0) {
+        result.push(array[i]);
+    }
+}
+console.log(result);
 
 //? Ex.4 (Hard) - Dato un array di numeri interi (nums) ed un numero intero (target), ritornare i 2 indici degli elementi nell’array nums che, sommati, restituiscano il numero target.
 //? Assumere che ogni nums fornito in input accetti solo una soluzione e che questo contenga solo numeri diversi fra loro.
